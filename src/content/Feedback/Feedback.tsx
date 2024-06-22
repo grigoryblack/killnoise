9cc15a3c5f4f47ee740ca0cfd05586022a90a2c7
Grigory Druzhenkov <druzhenkovgrigory@simple-it.pro>
2024-06-22 19:30:18 +0500

    change to prod url and add loader

--- src/content/Feedback/Feedback.tsx
+++ src/content/Feedback/Feedback.tsx
@@ -1,22 +1,28 @@
 import './index.scss';
 import Worker from "../../assets/img/worker.jpg";
-import {Form, Input, message} from 'antd';
+import {Form, Input, message, Spin} from 'antd';
 import axios from 'axios';
+import { useState } from 'react';
 
 const Feedback = () => {
     const [form] = Form.useForm();
+    const [loading, setLoading] = useState(false); // Состояние для загрузчика
 
     const onFinish = async (values: any) => {
+        setLoading(true); // Включение загрузчика
         try {
-            const response = await axios.post('http://localhost:8080/send-email', values);
+            const response = await axios.post('https://killnoise-backend.onrender.com/send-email', values);
             if (response.status === 200){
-                message.success('Спасибо за обращение!')
-                form.resetFields()
+                message.success('Спасибо за обращение!');
+                form.resetFields();
             } else {
-                message.warning('Не удалось отправить форму')
+                message.warning('Не удалось отправить форму');
             }
         } catch (error) {
             console.error('There was an error sending the email: ', error);
+            message.error('Произошла ошибка при отправке формы');
+        } finally {
+            setLoading(false); // Выключение загрузчика
         }
     };
 
@@ -32,46 +38,48 @@ const Feedback = () => {
                                 <br/><br/>
                                 Пожалуйста, заполните форму ниже, чтобы мы могли связаться с вами как можно скорее.
                             </p>
-                            <Form
-                                form={form}
-                                name="feedback"
-                                onFinish={onFinish}
-                                layout="vertical"
-                            >
-                                <Form.Item
-                                    label="Имя"
-                                    name="name"
-                                    rules={[{required: true, message: 'Пожалуйста, введите ваше имя'}]}
+                            <Spin spinning={loading}>
+                                <Form
+                                    form={form}
+                                    name="feedback"
+                                    onFinish={onFinish}
+                                    layout="vertical"
                                 >
-                                    <Input size="large"/>
-                                </Form.Item>
-                                <Form.Item
-                                    label="Номер телефона"
-                                    name="phone"
-                                    rules={[{required: true, message: 'Пожалуйста, введите ваш номер телефона'}]}
-                                >
-                                    <Input size="large"/>
-                                </Form.Item>
-                                <Form.Item
-                                    label="Почта"
-                                    name="email"
-                                    rules={[{required: true, message: 'Пожалуйста, введите вашу почту', type: 'email'}]}
-                                >
-                                    <Input size="large"/>
-                                </Form.Item>
-                                <Form.Item
-                                    label="Комментарий"
-                                    name="comment"
-                                    rules={[{required: true, message: 'Пожалуйста, введите ваш комментарий'}]}
-                                >
-                                    <Input.TextArea autoSize={{minRows: 2, maxRows: 6}}/>
-                                </Form.Item>
-                                <Form.Item>
-                                    <button className="form-button" type="submit">
-                                        Отправить
-                                    </button>
-                                </Form.Item>
-                            </Form>
+                                    <Form.Item
+                                        label="Имя"
+                                        name="name"
+                                        rules={[{required: true, message: 'Пожалуйста, введите ваше имя'}]}
+                                    >
+                                        <Input size="large" disabled={loading}/>
+                                    </Form.Item>
+                                    <Form.Item
+                                        label="Номер телефона"
+                                        name="phone"
+                                        rules={[{required: true, message: 'Пожалуйста, введите ваш номер телефона'}]}
+                                    >
+                                        <Input size="large" disabled={loading}/>
+                                    </Form.Item>
+                                    <Form.Item
+                                        label="Почта"
+                                        name="email"
+                                        rules={[{required: true, message: 'Пожалуйста, введите вашу почту', type: 'email'}]}
+                                    >
+                                        <Input size="large" disabled={loading}/>
+                                    </Form.Item>
+                                    <Form.Item
+                                        label="Комментарий"
+                                        name="comment"
+                                        rules={[{required: true, message: 'Пожалуйста, введите ваш комментарий'}]}
+                                    >
+                                        <Input.TextArea autoSize={{minRows: 2, maxRows: 6}} disabled={loading}/>
+                                    </Form.Item>
+                                    <Form.Item>
+                                        <button className="form-button" type="submit" disabled={loading}>
+                                            Отправить
+                                        </button>
+                                    </Form.Item>
+                                </Form>
+                            </Spin>
                         </div>
                     </div>
                     <div className="contact-inner__item">
